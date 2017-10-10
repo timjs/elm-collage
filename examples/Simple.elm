94c333c28807dedfa456f9190d56f17755784d64
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2017-10-10 13:15:29 +0200

    add simple example and its output

---
+++ examples/Simple.elm
@@ -0,0 +1,21 @@
+module Simple exposing (main)
+
+import Collage exposing (circle, filled, rectangle, uniform)
+import Collage.Layout exposing (at, topLeft)
+import Collage.Render exposing (svg)
+import Color
+
+
+main =
+    let
+        circ =
+            circle 50
+                |> filled (uniform Color.red)
+
+        rect =
+            rectangle 200 100
+                |> filled (uniform Color.blue)
+    in
+    rect
+        |> at topLeft circ
+        |> svg

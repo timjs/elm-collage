f108a75b3c59f936108e398da4eb9c90222d4953
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2017-10-11 12:16:06 +0200

    stylistic fix

--- src/Collage/Text.elm
+++ src/Collage/Text.elm
@@ -434,7 +434,7 @@ line line (Core.Chunk style str) =
 
 -}
 width : Text -> Float
-width (Core.Chunk style string) =
+width ((Core.Chunk style string) as text) =
     -- height text / 2 * toFloat (String.length string)
     Native.Text.width (toCssFontSpec style) string
 

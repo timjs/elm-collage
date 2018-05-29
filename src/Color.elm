765016dc95f1b881b803ffce29ce9a87e75708a8
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2018-05-29 15:17:31 +0200

    add stubs for color module

---
+++ src/Color.elm
@@ -0,0 +1,25 @@
+module Color exposing (Color, black, red, toRgb)
+
+
+type Color
+    = Color
+
+
+toRgb : Color -> { red : Int, green : Int, blue : Int, alpha : Float }
+toRgb =
+    Debug.todo "missing function in 0.19"
+
+
+red : Color
+red =
+    Debug.todo "missing function in 0.19"
+
+
+blue : Color
+blue =
+    Debug.todo "missing function in 0.19"
+
+
+black : Color
+black =
+    Debug.todo "missing function in 0.19"

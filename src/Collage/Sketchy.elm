bf970479b8f81cd3a98b088ec2a2a727950e826c
Chip Jackson <chipjacks@gmail.com>
2021-05-20 10:25:21 -0700

    Elm format sketchy module.

--- src/Collage/Sketchy.elm
+++ src/Collage/Sketchy.elm
@@ -1,4 +1,4 @@
-module Collage.Sketchy exposing (sketchy, defaultConfig, Config)
+module Collage.Sketchy exposing (Config, defaultConfig, sketchy)
 
 import Collage exposing (Collage, Point)
 import Collage.Core as Core
@@ -43,8 +43,8 @@ sketchy config collage =
                         |> Random.map2
                             (\points lines ->
                                 Collage.group <|
-                                    (List.map (\segment -> { collage | basic = Core.Path line (Core.Curve segment) }) lines)
-                                    ++ [ { collage | basic = Core.Shape ( fill, Collage.invisible ) (Core.Polygon points) } ]
+                                    List.map (\segment -> { collage | basic = Core.Path line (Core.Curve segment) }) lines
+                                        ++ [ { collage | basic = Core.Shape ( fill, Collage.invisible ) (Core.Polygon points) } ]
                             )
                             (sketchPoints config ps)
 
@@ -56,14 +56,13 @@ sketchy config collage =
                             , ( w / 2, h / 2 )
                             , ( -w / 2, h / 2 )
                             ]
-
                     in
                     sketchLines config ps
                         |> Random.map2
                             (\points lines ->
                                 Collage.group <|
-                                    (List.map (\segment -> { collage | basic = Core.Path line (Core.Curve segment) }) lines)
-                                    ++ [ { collage | basic = Core.Shape ( fill, Collage.invisible ) (Core.Polygon points) } ]
+                                    List.map (\segment -> { collage | basic = Core.Path line (Core.Curve segment) }) lines
+                                        ++ [ { collage | basic = Core.Shape ( fill, Collage.invisible ) (Core.Polygon points) } ]
                             )
                             (sketchPoints config ps)
 
@@ -75,9 +74,9 @@ sketchy config collage =
                     Random.map2
                         (\points1 points2 ->
                             Collage.group <|
-                                [ { collage | basic = Core.Path line (Core.Curve (points1 ++ (rotate points2))) }
+                                [ { collage | basic = Core.Path line (Core.Curve (points1 ++ rotate points2)) }
                                 ]
-                                ++ [ { collage | basic = Core.Shape ( fill, Collage.invisible ) (Core.Circle r) } ]
+                                    ++ [ { collage | basic = Core.Shape ( fill, Collage.invisible ) (Core.Circle r) } ]
                         )
                         (sketchPoints { config | bowing = 0 } ps)
                         (sketchPoints { config | bowing = 0 } ps)
@@ -90,9 +89,9 @@ sketchy config collage =
                     Random.map2
                         (\points1 points2 ->
                             Collage.group <|
-                                [ { collage | basic = Core.Path line (Core.Curve (points1 ++ (rotate points2))) }
+                                [ { collage | basic = Core.Path line (Core.Curve (points1 ++ rotate points2)) }
                                 ]
-                                ++ [ { collage | basic = Core.Shape ( fill, Collage.invisible ) (Core.Ellipse rx ry) } ]
+                                    ++ [ { collage | basic = Core.Shape ( fill, Collage.invisible ) (Core.Ellipse rx ry) } ]
                         )
                         (sketchPoints { config | bowing = 0 } ps)
                         (sketchPoints { config | bowing = 0 } ps)
@@ -114,9 +113,11 @@ sketchy config collage =
             Random.constant collage
 
 
+
 -- INTERNAL
 
-segments : Bool -> List Point -> List (Point, Point)
+
+segments : Bool -> List Point -> List ( Point, Point )
 segments closed ps =
     List.map2
         Tuple.pair
@@ -124,22 +125,26 @@ segments closed ps =
         (List.drop 1 ps ++ List.take (List.length ps - 1) ps)
         |> (if closed then
                 List.take (List.length ps)
+
             else
-                List.take ((List.length ps) - 1))
+                List.take (List.length ps - 1)
+           )
+
 
 rotate : List a -> List a
 rotate list =
-    case list  of
+    case list of
         head :: tail ->
             tail ++ [ head ]
 
         _ ->
             list
 
+
 sketchLines : Config -> List Point -> Random.Generator (List (List Point))
 sketchLines config ps =
     segments True ps
-        |> List.concatMap (\(a, b) -> [ sketchPoints config [ a, b ], sketchPoints config [ a, b ] ])
+        |> List.concatMap (\( a, b ) -> [ sketchPoints config [ a, b ], sketchPoints config [ a, b ] ])
         |> Random.Extra.combine
 
 
@@ -149,6 +154,7 @@ sketchPoints config ps =
         bowedPs =
             if config.bowing == 0 then
                 ps
+
             else
                 List.map2
                     (\( x1, y1 ) ( x2, y2 ) -> [ ( x1, y1 ), ( x1 + (x2 - x1) / 2, y1 + (y2 - y1) / 2 ) ])
@@ -159,7 +165,7 @@ sketchPoints config ps =
 
         lineLength =
             List.map2
-                (\( x1, y1 ) ( x2, y2 ) -> ( x2 - x1 ) ^ 2 + (y2 - y1) ^ 2 |> sqrt)
+                (\( x1, y1 ) ( x2, y2 ) -> (x2 - x1) ^ 2 + (y2 - y1) ^ 2 |> sqrt)
                 ps
                 (List.drop 1 ps ++ List.take (List.length ps - 1) ps)
                 |> List.take (List.length ps)
@@ -168,14 +174,17 @@ sketchPoints config ps =
         roughness =
             (if lineLength > 200 then
                 1
-            else if lineLength > 500 then
+
+             else if lineLength > 500 then
                 0.4
-            else
-                (lineLength / 100)) * config.roughness
+
+             else
+                lineLength / 100
+            )
+                * config.roughness
 
         randomOffset =
             Random.pair (Random.float (0 - roughness) roughness) (Random.float (0 - roughness) roughness)
-
     in
     Random.list (List.length bowedPs) randomOffset
         |> Random.map
@@ -193,17 +202,15 @@ ellipsePoints : Float -> Float -> List Point
 ellipsePoints rx ry =
     let
         m r =
-            (r ^ 2 / 2 |> sqrt)
+            r ^ 2 / 2 |> sqrt
     in
-    [ (0, ry)
-    , (-(m rx), m ry)
-    , (-rx, 0)
-    , (-(m rx), -(m ry))
-    , (0, -ry)
-    , (m rx, -(m ry))
-    , (rx, 0)
-    , (m rx, m ry)
-    , (0, ry)
+    [ ( 0, ry )
+    , ( -(m rx), m ry )
+    , ( -rx, 0 )
+    , ( -(m rx), -(m ry) )
+    , ( 0, -ry )
+    , ( m rx, -(m ry) )
+    , ( rx, 0 )
+    , ( m rx, m ry )
+    , ( 0, ry )
     ]
-
-
